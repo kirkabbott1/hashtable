b67914441837f6572d4fd416b87722d348d4fac1
Kirk Abbott <kirkabbott1@gmail.com>
2017-02-05 17:05:40 -0500

    adds get method prac

--- hashTablePrac.js
+++ hashTablePrac.js
@@ -20,25 +20,58 @@ HashTable.prototype.hash = function(key) {
 
 HashTable.prototype.insert = function(key, val) {
   var index = this.hash(key);
-  console.log('INDEX:', index);
-  if (!this.buckets[index]) this.buckets[index] = new HashNode(key, val);
+  // console.log('INDEX:', index);
+  if (!this.buckets[index]) {
+     this.buckets[index] = new HashNode(key, val);
+  }
+  else if (this.buckets[index].key === key) {
+    this.buckets[index].value = val;
+  }
   else {
     var currentNode = this.buckets[index];
     while (currentNode.next) {
+      if (currentNode.next.key === key) {
+        currentNode.next.value = val;
+        return;
+      }
       currentNode= currentNode.next;
     }
     currentNode.next = new HashNode(key, val);
   }
-}
+};
+
+HashTable.prototype.get = function(key) {
+  var index = this.hash(key);
+  if (!this.buckets[index]) {
+    return null;
+  } else {
+    var currentNode = this.buckets[index];
+    while (currentNode) {
+      if (currentNode.key === key) {
+        return currentNode.value;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+};
 
 var myHT = new HashTable(30);
 
 myHT.insert('Dean', 'dean@gmail.com');
 myHT.insert('Megan', 'meg@gmail.com');
 myHT.insert('Dane', 'dane@yahoo.com');
+myHT.insert('Dean', 'deanguy@gmail.com');
+myHT.insert('Megan', 'megansmith@gmail.com');
+myHT.insert('Dane', 'dane1010@outlook.com');
 
 
-console.log(myHT.buckets);
+console.log(myHT.get('Dean'));
+console.log(myHT.get('Megan'));
+console.log(myHT.get('Dane'));
+
+
+// console.log(myHT.buckets);
 
 // console.log(myHT.hash('Kirk'));
 
